559431abb05884d10d98fc87e5c1c01cec9ca32d
Chandana <chandanag.acharya.gmail.com>
2026-09-10 23:11:38 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.json({ message: 'Server UP' ,
+    server : 'Order-Management-Platform'
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+}); 
